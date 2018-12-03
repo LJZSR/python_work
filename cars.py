d070e82919bafef89af11b98b3cf55e807932cec
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-03 16:23:09 +0800

    Create cars.py

---
+++ cars.py
@@ -0,0 +1,17 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print('\nHere is the original list:')
+print(cars)
+print('\nHere is the sorted list:')
+print(sorted(cars))
+print('\nHere is the original list again:')
+print(cars)
+print('\nHere is the reversed-sorted list:')
+print(sorted(cars, reverse=True))
+cars.reverse()
+print(cars)
+print(len(cars))
