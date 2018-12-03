9244e81f9c30cc7a2968468a12652f88d4ca6d6e
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-03 17:32:24 +0800

    Create foods.py

---
+++ foods.py
@@ -0,0 +1,20 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print('My favorite foods are:')
+print(my_foods)
+print("\nMy friend's favorite foods are")
+print(friend_foods) 
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print('\nMy favorite foods are:')
+print(my_foods)
+print("\nMy friend's favorite foods are")
+print(friend_foods) 
+#这样不行
+friend_foods = my_foods
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print('\nMy favorite foods are:')
+print(my_foods)
+print("\nMy friend's favorite foods are")
+print(friend_foods) 
