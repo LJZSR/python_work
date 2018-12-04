e0b2307736b85ae1d5973536548780b901cad4a7
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-04 22:51:05 +0800

    Update toppings.py

--- toppings.py
+++ toppings.py
@@ -1,3 +1,8 @@
-requested_topping = 'mushrooms'
-if requested_topping != 'anchovies':
-	print('Hold the anchovies!')
+requested_topping = ['mushrooms', 'extra chess']
+
+if 'mushrooms' in requested_topping:
+	print('Adding mushrooms.')
+if 'pepperoni' in requested_topping:
+	print('Adding pepperoni.')
+if 'extra chess' in requested_topping:
+	print('Adding extra chess.')
