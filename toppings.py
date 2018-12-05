74c8ef74046cbf055599eb69a8bf62c160d9cf51
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-05 18:40:12 +0800

    Update toppings.py

--- toppings.py
+++ toppings.py
@@ -1,8 +1,11 @@
-requested_toppings = ['mushrooms', 'green peppers', 'extra chess']
+available_toppings = ['mushrooms', 'olives', 'green peppers', 
+					  'pepperoni', 'pineapple', 'extra chess']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra chess']
 
 for requested_topping in requested_toppings:
-	if requested_topping == 'green peppers':
-		print('Sorry, we are out of green peppers right now.')
+	if requested_topping not in available_toppings:
+		print('Sorry, we are out of ' + requested_topping + ' right now.')
 	else:
 		print('Adding ' + requested_topping + '.')
 print('\nFinished making your pizza!')
