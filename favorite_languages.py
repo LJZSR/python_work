ed1820d2d2fea7ea136a2413c01db7cd31f7a7e9
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-07 20:49:36 +0800

    Update favorite_languages.py

--- favorite_languages.py
+++ favorite_languages.py
@@ -12,4 +12,14 @@ print("Sarah's favorite language is " +
 for name, language in favorite_languages.items():
 	print('\n' + name.title() + "'s favorite language is " +
 		language.title() + '.')
-	
+
+for name in favorite_languages.keys():
+	print(name.title())
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+	print(name.title())
+	if name in friends:
+		print('	Hi ' + name.title() +
+			', I see your favorite language is ' +
+			favorite_languages[name].title() + '!')
