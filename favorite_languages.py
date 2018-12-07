6cc3256a5f6b2bee24bb0d1138144e36c218ae27
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-07 21:21:29 +0800

    Update favorite_languages.py

--- favorite_languages.py
+++ favorite_languages.py
@@ -1,29 +1,12 @@
 favorite_languages = {
-	'jen': 'python',
-	'sarah': 'c',
-	'edward': 'ruby',
-	'phil': 'python'
+	'jen': ['python', 'ruby'],
+	'sarah': ['c'],
+	'edward': ['ruby', 'go'],
+	'phil': ['python', 'haskell'],
     }
 
-print("Sarah's favorite language is " +
-	favorite_languages['sarah'].title() +
-	'.')
 
-for name, language in favorite_languages.items():
-	print('\n' + name.title() + "'s favorite language is " +
-		language.title() + '.')
-
-for name in favorite_languages.keys():
-	print(name.title())
-
-friends = ['phil', 'sarah']
-for name in favorite_languages.keys():
-	print(name.title())
-	if name in friends:
-		print('	Hi ' + name.title() +
-			', I see your favorite language is ' +
-			favorite_languages[name].title() + '!')
-
-print('\nThe following language have been mentioned:')
-for language in sorted(set(favorite_languages.values())):
-	print(language.title())
+for name, languages in favorite_languages.items():
+	print('\n' + name.title() + "'s favorite languages are:")
+	for language in languages:
+		print('\t' + language.title())
