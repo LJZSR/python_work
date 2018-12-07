b187bb14f6f94ed04308b31bb28b7faa95e1707a
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-07 21:11:29 +0800

    Update aliens.py

--- aliens.py
+++ aliens.py
@@ -8,8 +8,18 @@ print(aliens)
 
 aliens =[]
 for i in range(0,30):
-	new_alien = {'color': 'green', 'points': 5}
+	new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
 	aliens.append(new_alien)
+
+for alien in aliens[:3]:
+	if alien['color'] == 'green':
+		alien['color'] = 'yellow'
+		alien['speed'] = 'medium'
+		alien['points'] = 10
+	elif alien['color'] =='yellow':
+		alien['color'] = 'red'
+		alien['speed'] = 'fast'
+		alien['points'] = 15
 
 #显示前5个外星人
 for alien in aliens[:5]:
