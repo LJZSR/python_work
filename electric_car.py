103b12f72963eb229a2011d33ab2a097042cbad6
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-09 22:21:54 +0800

    Update electric_car.py

--- electric_car.py
+++ electric_car.py
@@ -31,19 +31,38 @@ class Car():
 		"""将里程表度数增加指定的量"""
 		self.odometer_reading += miles
 
+class Battery():
+	"""一次模拟电动汽车电瓶的简单尝试"""
+
+	def __init__(self, battery_size=70):
+		"""初始化电瓶属性"""
+		self.battery_size = battery_size
+
+	def describe_battery(self):
+		"""打印一条描述电瓶容量的信息"""
+		print('This car has a ' + str(self.battery_size) + '-kWh battery.')
+
+	def get_range(self):
+		"""打印一条消息，指出电瓶的续航里程"""
+		if self.battery_size == 70:
+			range = 240
+		elif self.battery_size ==85:
+			range = 270
+		message = 'This car can go approximately ' + str(range)
+		message += ' miles in a full charge.'
+		print(message)
+
 class ElectricCar(Car):
 	"""电动汽车的独特之处"""
 
 	def __init__(self, make, model, year):
 		"""初始化父类的属性，再初始化电动汽车的特有属性"""
 		super().__init__(make, model, year)
-		self.battery_size = 70
-
-	def describe_battery(self):
-		"""打印一条描述电瓶容量的信息"""
-		print('This car has a ' + str(self.battery_size) + '-kWh battery.')
+		self.battery = Battery()
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+
 		
