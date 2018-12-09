3e0494d6d37312bd336f7cba8af52d3002db29dc
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-09 13:01:24 +0800

    Update pets.py

--- pets.py
+++ pets.py
@@ -1,7 +1,7 @@
-def describe_pet(animal_tpye, pet_name):
+def describe_pet(pet_name, animal_tpye='dog'):
 	"""显示宠物信息"""
 	print('\nI have a ' + animal_tpye + '.')
 	print('My ' + animal_tpye + "'s name is " + pet_name.title() + '.')
 
-describe_pet('hamster', 'harry')
-describe_pet('dog', 'willie')
+describe_pet('harry', 'hamster')
+describe_pet('willie')
