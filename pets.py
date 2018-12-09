c803c748c42951657b3b14bcbd92ee659711dde5
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-09 12:57:30 +0800

    Update pets.py

--- pets.py
+++ pets.py
@@ -1,7 +1,7 @@
-pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
-print (pets)
+def describe_pet(animal_tpye, pet_name):
+	"""显示宠物信息"""
+	print('\nI have a ' + animal_tpye + '.')
+	print('My ' + animal_tpye + "'s name is " + pet_name.title() + '.')
 
-while 'cat' in pets:
-	pets.remove('cat')
-
-print(pets)
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
