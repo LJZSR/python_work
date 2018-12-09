0617272c235a5aaf44b6eb5432f8b1277db6d831
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-09 20:42:29 +0800

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -9,3 +9,12 @@ print('You ordered a ' + pizza['crust'] + '-crust pizza ' +
 for topping in pizza['toppings']:
 	print('\t' + topping)
 	
+def  make_pizza(size, *toppings):
+	"""概述要制作的比萨"""
+	print('\nMake a ' + str(size) +
+		'-inch pizza with the folling toppings:')
+	for topping in toppings:
+		print('-' + topping)
+
+make_pizza(16, 'peperoni')
+make_pizza(12, 'mushroom', 'green peppers', 'extra cheese')
