57eef6f59c4e5c0fdbbc34bc55ebdab2db797270
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-10 22:12:49 +0800

    Update file_reader.py

--- file_reader.py
+++ file_reader.py
@@ -1,5 +1,7 @@
 file_name = 'pi_digits.txt'
 
 with open(file_name) as file_object:
-	for line in file_object:
-		print(line.rstrip())
+	lines = file_object.readlines()
+
+for line in lines:
+	print(line.rstrip())
