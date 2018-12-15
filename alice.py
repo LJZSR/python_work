c57ac5d86766891ba7db2aabe08723d958f409e9
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-15 16:47:12 +0800

    Update alice.py

--- alice.py
+++ alice.py
@@ -6,3 +6,8 @@ try:
 except FileNotFoundError:
 	msg = 'Sorry, the file ' + file_name + ' does not exist!'
 	print(msg)
+else:
+	#计算文件大致包含多少单词
+	words = contents.split()
+	num_words = len(words)
+	print('The file ' + file_name + ' has about ' + str(num_words) + ' words.')
