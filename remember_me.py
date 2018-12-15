c902255893ac5129d936845b00858d5b2a8fa8a4
LJZSR <jzs1996@sjtu.edu.cn>
2018-12-15 17:18:03 +0800

    Update remember_me.py

--- remember_me.py
+++ remember_me.py
@@ -1,8 +1,16 @@
 import json
 
-username = input('What is your name? ')
+#如果以前存储了用户名，就加载它
+#否则，就提示用户输入用户名并存储它
 
 file_name = 'username.json'
-with open(file_name, 'w') as f_object:
-	json.dump(username, f_object)
-	print("We'll remember you when you come back, " + username + '!')
+try:
+	with open(file_name) as f_object:
+		username = json.load(f_object)
+except FileNotFoundError:
+	username = input('What is your name? ')
+	with open(file_name, 'w') as f_object:
+		json.dump(username, f_object)
+		print("We'll remember you when you come back, " + username + '!')
+else:
+	print('Welcome back, ' + username + '!')
